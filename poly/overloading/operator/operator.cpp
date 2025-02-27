7b589050bf56cd6b85a933d7565cd35e1387524e
panha <hkmkh26@gmail.com>
2025-02-27 01:38:26 -0800

    update

--- poly/overloading/operator/operator.cpp
+++ poly/overloading/operator/operator.cpp
@@ -1,35 +1,73 @@
-#include <iostream>
+// #include <iostream>
 
+// using namespace std;
+// class Counter{
+//     private:
+//         int counter;
+//     public:
+//         Counter()
+//         {
+//             counter = 0;
+//         }
+//         void increment()
+//         {
+//             counter++;
+//         }
+//         void decrement()
+//         {
+//             counter--;
+//         }
+//         int getCounter()
+//         {
+//             return counter;
+//         }
+// };
+// int main()
+// {
+//     Counter c1, c2;
+//     c1.increment();
+//     c1.increment();
+//     c1.increment();
+//     c1.increment();
+//     c1.decrement();
+//     cout<<"|+|0"<<c1.getCounter()<<"|-|"<<endl;
+//     return 0;
+// }
+#include <iostream>
+#include <conio.h>
 using namespace std;
-class Counter{
-    private:
-        int counter;
-    public:
-        Counter()
-        {
-            counter = 0;
-        }
-        void increment()
-        {
-            counter++;
-        }
-        void decrement()
-        {
-            counter--;
-        }
-        int getCounter()
-        {
-            return counter;
-        }
+
+class Counter {
+private:
+    int counter;
+public:
+    Counter() { counter = 0; }
+    void increment() { counter++; }
+    void decrement() { counter--; }
+    int getCounter() { return counter; }
 };
-int main()
-{
-    Counter c1, c2;
-    c1.increment();
-    c1.increment();
-    c1.increment();
-    c1.increment();
-    c1.decrement();
-    cout<<"|+|0"<<c1.getCounter()<<"|-|"<<endl;
+
+int main() {
+    Counter c;
+    char choice;
+    system("cls");
+    do {
+        cout << "\nCounter: " << c.getCounter() << endl;
+        cout << "1. Increment (+)\n2. Decrement (-)\n3. Exit (x)\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        if (choice == '1' || choice == '+') {
+            c.increment();
+        } else if (choice == '2' || choice == '-') {
+            c.decrement();
+        } else if (choice == 'x') {
+            break;
+        } else {
+            cout << "Invalid input! Try again.\n";
+        }
+    } while (true);
+
+    cout << "Final Counter Value: " << c.getCounter() << endl;
     return 0;
 }
